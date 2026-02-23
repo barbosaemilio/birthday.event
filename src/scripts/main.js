dcbab89185fe3b2026d421d623cb78ece44af266
barbosaemilio <barbosaemiliob@gmail.com>
2026-02-23 16:20:34 -0300

    contagem evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("ola parcel")
+const dataDoEvento = new Date("dec 10, 2026 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const dias = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horas = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutos = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundos = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML =
+        `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = "🎉 A Festa Começou!";
+    }
+
+}, 1000);
